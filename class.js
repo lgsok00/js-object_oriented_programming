fb7cabdfab16d9701deb6871867a035ebd50801f
lgsok00 <lgsok00@naver.com>
2023-10-05 14:25:13 +0900

    Method

--- class.js
+++ class.js
@@ -3,15 +3,17 @@ class Person {
     this.name = name;
     this.first = first;
     this.second = second;
-    console.log('constructor');
+  }
+  sum = function () {
+    return 'prototype : ' + (this.first + this.second);
   }
 }
 
 var kim = new Person('kim', 10, 20);
-console.log('kim', kim);
-// kim.sum = function () {
-//   return 'this : ' + (this.first + this.second);
-// }
-// var lee = new Person('lee', 10, 10);
-// console.log("kim.sum()", kim.sum());
-// console.log("lee.sum()", lee.sum());
+kim.sum = function () {
+  return 'this : ' + (this.first + this.second);
+}
+
+var lee = new Person('lee', 10, 10);
+console.log("kim.sum()", kim.sum());
+console.log("lee.sum()", lee.sum());

3b13f4247f3cd065eb8ce699fdf42cade5f6de17
lgsok00 <lgsok00@naver.com>
2023-10-05 17:24:59 +0900

    constructor inheritance

--- constructor-inheritance.js
+++ constructor-inheritance.js
@@ -11,6 +11,11 @@ function PersonPlus(name, first, second, third) {
   Person.call(this, name, first, second);
   this.third = third;
 }
+
+// PersonPlus.prototype.__proto__ = Person.prototype;
+PersonPlus.prototype = Object.create(Person.prototype);
+PersonPlus.prototype.constructor = PersonPlus;
+
 PersonPlus.prototype.avg = function () {
   return (this.first + this.second + this.third) / 3;
 }
@@ -18,3 +23,4 @@ PersonPlus.prototype.avg = function () {
 var kim = new PersonPlus('kim', 10, 20, 30);
 console.log("kim.sum()", kim.sum());
 console.log("kim.avg()", kim.avg());
+console.log("kim.constructor", kim.constructor);

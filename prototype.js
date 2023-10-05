b6217cf8984e5319822a3b294baa7ec126e28ca5
lgsok00 <lgsok00@naver.com>
2023-10-05 14:04:07 +0900

    prototype2

--- prototype.js
+++ prototype.js
@@ -1,17 +1,17 @@
-function Person(name, first, second, third) {
+function Person(name, first, second) {
   this.name = name;
   this.first = first;
   this.second = second;
-  this.sum = function () {
-    return this.first + this.second;
-  }
+}
+
+Person.prototype.sum = function () {
+  return 'prototype : ' + (this.first + this.second);
 }
 
 var kim = new Person('kim', 10, 20);
 kim.sum = function () {
-  return 'modified : ' + (this.first + this.second);
+  return 'this : ' + (this.first + this.second);
 }
 var lee = new Person('lee', 10, 10);
-
 console.log("kim.sum()", kim.sum());
 console.log("lee.sum()", lee.sum());

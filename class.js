ac980a7fbfd9222043c36ee06ddd5f92687e5f88
lgsok00 <lgsok00@naver.com>
2023-10-05 14:42:27 +0900

    inheritance

--- class.js
+++ class.js
@@ -4,16 +4,24 @@ class Person {
     this.first = first;
     this.second = second;
   }
-  sum = function () {
-    return 'prototype : ' + (this.first + this.second);
+  sum() {
+    return this.first + this.second;
   }
 }
 
-var kim = new Person('kim', 10, 20);
-kim.sum = function () {
-  return 'this : ' + (this.first + this.second);
+class PersonPlus extends Person {
+  constructor(name, first, second, third) {
+    super(name, first, second);
+    this.third = third;
+  }
+  sum() {
+    return super.sum() + this.third;
+  }
+  avg() {
+    return (this.first + this.second + this.third) / 3;
+  }
 }
 
-var lee = new Person('lee', 10, 10);
+var kim = new PersonPlus('kim', 10, 20, 30);
 console.log("kim.sum()", kim.sum());
-console.log("lee.sum()", lee.sum());
+console.log("kim.avg()", kim.avg());

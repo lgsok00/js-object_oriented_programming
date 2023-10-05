908e9ca73c6a6f2d709a9c64079cb1472a1927e7
lgsok00 <lgsok00@naver.com>
2023-10-05 14:24:32 +0900

    class_constructor_function

--- class.js
+++ class.js
@@ -1,11 +1,13 @@
 class Person {
-
+  constructor(name, first, second) {
+    this.name = name;
+    this.first = first;
+    this.second = second;
+    console.log('constructor');
+  }
 }
 
-
-
-
-var kim = new Person();
+var kim = new Person('kim', 10, 20);
 console.log('kim', kim);
 // kim.sum = function () {
 //   return 'this : ' + (this.first + this.second);
